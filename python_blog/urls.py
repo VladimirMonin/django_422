f57593c995a236b7f2777f4d4f6b95e805b613c4
VladimirMonin <vladimir.monin2016@yandex.ru>
2024-12-09 21:44:22 +0500

    lesson_54: маршрутизация и функция обработки

--- python_blog/urls.py
+++ python_blog/urls.py
@@ -1,19 +1,20 @@
 # python_blog\urls.py Django APP python_blog
 from django.contrib import admin
 from django.urls import path
-from python_blog.views import catalog_posts, post_detail, catalog_categories, category_detail, catalog_tags, tag_detail,
+from python_blog.views import catalog_posts, post_detail, catalog_categories, category_detail, catalog_tags, tag_detail
 
 
 urlpatterns = [
+    # Посты
     path('', catalog_posts),
-    path('<slug:slug>/', post_detail), # /posts/osnovy-python/
+    path('<slug:post_slug>/', post_detail), # /posts/osnovy-python/
    
    # Категории
     path('categories/', catalog_categories),
-    path('categories/<slug:slug>/', category_detail),
+    path('categories/<slug:category_slug>/', category_detail),
    
    # Теги
     path('tags/', catalog_tags),
-    path('tags/<slug:slug>/', tag_detail),
+    path('tags/<slug:tag_slug>/', tag_detail),
 
 ]

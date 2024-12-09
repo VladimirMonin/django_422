fb99ba92bad4af668a9fc9c0b476b7b067c2e672
VladimirMonin <vladimir.monin2016@yandex.ru>
2024-12-09 23:00:46 +0500

    lesson_54: reverse и базовая навигация в категориях

--- python_blog/views.py
+++ python_blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
+
+CATEGORIES = [
+    {'slug': 'python', 'name': 'Python'},
+    {'slug': 'django', 'name': 'Django'},
+    {'slug': 'postgresql', 'name': 'PostgreSQL'},
+    {'slug': 'docker', 'name': 'Docker'},
+    {'slug': 'linux', 'name': 'Linux'},
+]
 
 def main(request):
     return HttpResponse('Главная страница')
@@ -12,10 +21,30 @@ def post_detail(request, post_slug):
     return HttpResponse(f'Страница поста {post_slug}')
 
 def catalog_categories(request):
-    return HttpResponse('Каталог категорий')
+    links = []
+    for category in CATEGORIES:
+        url = reverse('category_detail', args=[category['slug']])
+        links.append(f'<p><a href="{url}">{category["name"]}</a></p>')
+    
+    return HttpResponse(f"""
+        <h1>Каталог категорий</h1>
+        {''.join(links)}
+        <p><a href="{reverse('posts')}">К списку постов</a></p>
+    """)
 
 def category_detail(request, category_slug):
-    return HttpResponse(f'Страница категории {category_slug}')
+
+    category = [cat for cat in CATEGORIES if cat['slug'] == category_slug][0]
+    
+    if category:
+        name = category['name']
+    else:
+        name = category_slug
+        
+    return HttpResponse(f"""
+        <h1>Категория: {name}</h1>
+        <p><a href="{reverse('categories')}">Назад к категориям</a></p>
+    """)
 
 def catalog_tags(request):
     return HttpResponse('Каталог тегов')

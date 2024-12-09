c80ef07f75fb01c9eec945cb2df515716e2f6aa3
VladimirMonin <vladimir.monin2016@yandex.ru>
2024-12-09 21:58:18 +0500

    lesson_54: упорядочили urls от специфичных к общим чтобы маршрутизация заработала правильно

--- python_blog/urls.py
+++ python_blog/urls.py
@@ -3,18 +3,22 @@ from django.contrib import admin
 from django.urls import path
 from python_blog.views import catalog_posts, post_detail, catalog_categories, category_detail, catalog_tags, tag_detail
 
-
+# Общий префикс posts/
 urlpatterns = [
-    # Посты
+    # Каталог  постов posts/
     path('', catalog_posts),
-    path('<slug:post_slug>/', post_detail), # /posts/osnovy-python/
-   
-   # Категории
+    
+    # Категории
+    # Категории posts/categories/
+    # Категории posts/categories/python/
     path('categories/', catalog_categories),
     path('categories/<slug:category_slug>/', category_detail),
-   
-   # Теги
+    
+    # Теги posts/tags/
+    # Теги posts/tags/python/
     path('tags/', catalog_tags),
     path('tags/<slug:tag_slug>/', tag_detail),
-
+    
+    # Посты posts/tags/
+    path('<slug:post_slug>/', post_detail),
 ]

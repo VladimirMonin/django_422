b423c0b8fda59e18b793871939d22425d56f3b22
VladimirMonin <vladimir.monin2016@yandex.ru>
2024-12-11 21:45:23 +0500

    lesson_55: первые переменные в шаблоне

--- python_blog/views.py
+++ python_blog/views.py
@@ -3,54 +3,68 @@ from django.http import HttpResponse
 from django.urls import reverse
 
 CATEGORIES = [
-    {'slug': 'python', 'name': 'Python'},
-    {'slug': 'django', 'name': 'Django'},
-    {'slug': 'postgresql', 'name': 'PostgreSQL'},
-    {'slug': 'docker', 'name': 'Docker'},
-    {'slug': 'linux', 'name': 'Linux'},
+    {"slug": "python", "name": "Python"},
+    {"slug": "django", "name": "Django"},
+    {"slug": "postgresql", "name": "PostgreSQL"},
+    {"slug": "docker", "name": "Docker"},
+    {"slug": "linux", "name": "Linux"},
 ]
 
-def main(request):
-    catalog_categories_url = reverse('blog:categories')
-    catalog_tags_url = reverse('blog:tags')
 
-    return render(request, "main.html")
+def main(request):
+    catalog_categories_url = reverse("blog:categories")
+    catalog_tags_url = reverse("blog:tags")
+
+    context = {
+        "title": "Главная страница",
+        "text": "Текст главной страницы",
+    }
+    return render(request, "main.html", context)
 
 
 def catalog_posts(request):
-    return HttpResponse('Каталог постов')
+    return HttpResponse("Каталог постов")
+
 
 def post_detail(request, post_slug):
-    return HttpResponse(f'Страница поста {post_slug}')
+    return HttpResponse(f"Страница поста {post_slug}")
+
 
 def catalog_categories(request):
     links = []
     for category in CATEGORIES:
-        url = reverse('blog:category_detail', args=[category['slug']])
+        url = reverse("blog:category_detail", args=[category["slug"]])
         links.append(f'<p><a href="{url}">{category["name"]}</a></p>')
-    
-    return HttpResponse(f"""
+
+    return HttpResponse(
+        f"""
         <h1>Каталог категорий</h1>
         {''.join(links)}
         <p><a href="{reverse('blog:posts')}">К списку постов</a></p>
-    """)
+    """
+    )
+
 
 def category_detail(request, category_slug):
 
-    category = [cat for cat in CATEGORIES if cat['slug'] == category_slug][0]
-    
+    category = [cat for cat in CATEGORIES if cat["slug"] == category_slug][0]
+
     if category:
-        name = category['name']
+        name = category["name"]
     else:
         name = category_slug
-        
-    return HttpResponse(f"""
+
+    return HttpResponse(
+        f"""
         <h1>Категория: {name}</h1>
         <p><a href="{reverse('blog:categories')}">Назад к категориям</a></p>
-    """)
+    """
+    )
+
 
 def catalog_tags(request):
-    return HttpResponse('Каталог тегов')
+    return HttpResponse("Каталог тегов")
+
 
 def tag_detail(request, tag_slug):
-    return HttpResponse(f'Страница тега {tag_slug}')
+    return HttpResponse(f"Страница тега {tag_slug}")

018f6ffd5fb3504dbe6daf8bfb88b2514fdc1952
VladimirMonin <vladimir.monin2016@yandex.ru>
2024-12-18 23:05:27 +0500

    hw_22: модификация базового шаблона и добавление данных блога

--- python_blog/views.py
+++ python_blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
+from .blog_data import dataset
 
 CATEGORIES = [
     {"slug": "python", "name": "Python"},

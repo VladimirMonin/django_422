19b92c203c57ba850288ad3ecec4cec27ffd8db3
VladimirMonin <vladimir.monin2016@yandex.ru>
2024-12-23 22:55:58 +0500

    lesson_58: цикл CRUD операций с таблицей Post

--- python_blog/models.py
+++ python_blog/models.py
@@ -6,3 +6,48 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.CharField(null=True, blank=True, max_length=100)
+
+
+# PRACTICE - Работа с моделью Post
+"""
+1. Создать новый пост
+post = Post(title="Django для чайников", content="Django очень лёгкий фреймворк, и у него пологая кривая входа...")
+post.save()
+
+INSERT INTO "python_blog_post" ("title", "content", "created_at", "updated_at", "category")
+VALUES ('Django для чайников', 'Django очень лёгкий фреймворк, и у него пологая кривая входа...', '2024-12-23 17:30:53.551384', '2024-12-23 17:30:53.551410', NULL) RETURNING "python_blog_post"."id"
+Execution time: 0.000827s [Database: default]
+
+
+post_1 = post
+
+post_2 = Post(title="Базы данных для продавцов котиков", content="Теперь вы сможете каждого котика сохранить в базе данных")
+
+Django ORM ленива.
+Все запросы по умолчанию выполняются в ленивом режиме.
+Допустим при сохранении, запрос будет выполнен только при вызове метода save()
+
+При чтении, запрос будет выполнен при обращении к результату запроса
+
+2. Получить все посты
+
+posts = Post.objects.all()
+posts = <QuerySet [<Post: Post object (1)>, <Post: Post object (2)>]>
+
+Что такое QuerySet?
+QuerySet - это набор объектов, которые мы получили из базы данных.
+Мы можем использовать QuerySet для фильтрации, сортировки, группировки и других операций над объектами.
+
+post_1 = posts[0]
+post_1
+
+post_1.title
+post_1.content
+
+post_1.content = "Django не очень лёгкий фреймворк, но у него пологая кривая входа..."
+
+post_3 = Post(title="Тестовый пост", content="Тестовый пост")
+
+# Делаем операцию удаления
+post_3.delete()
+"""

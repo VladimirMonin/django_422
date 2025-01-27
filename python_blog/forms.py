1fe5a207e4d48bc0ca424ccadc7483b582e877ec
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-01-27 23:55:25 +0500

    lesson_64: более сложная форма и шаблон для тега

--- python_blog/forms.py
+++ python_blog/forms.py
@@ -11,24 +11,22 @@ class TagForm(forms.Form):
         label="Название",
         help_text="Введите название тега",
         required=True,
+        error_messages={
+            "min_length": "Название должно быть не менее 2 символов",
+            "max_length": "Название должно быть не более 200 символов",
+            "required": "Название обязательно для заполнения",
+        },
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Например: Python',
+            'id': 'tag-name'
+        })
     )
 
     def clean_name(self):
         name = self.cleaned_data['name']
+        # Проверяем существование тега с таким именем
         if Tag.objects.filter(name=name).exists():
             raise forms.ValidationError('Тег с таким названием уже существует')
         return name
-
-    class Meta:
-        widgets = {
-            "name": forms.TextInput(attrs={"class": "form-control"}),
-        }
-
-        error_messages = {
-            "name": {
-                "min_length": "Название должно быть не менее 2 символов",
-                "max_length": "Название должно быть не более 200 символов",
-                "required": "Название обязательно для заполнения",
-            }
-        }
 

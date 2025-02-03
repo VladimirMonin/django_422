90798ce4e2a4aad927803ec50987ce1072b30a61
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-02-03 22:28:26 +0500

    lesson_66: обозначили базовые маршруты для пользователей и вью-заглушки

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,2 +1,12 @@
+from django.urls import path
+from users_app.views import login_user, register_user, logout_user, profile_user
 
-app_name = 'users'
+
+app_name = "users"
+
+urlpatterns = [
+    path("login/", login_user, name="login"),
+    path("register/", register_user, name="register"),
+    path("logout/", logout_user, name="logout"),
+    path("profile/", profile_user, name="profile"),
+]
--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login_user(request):
+    pass
+
+
+def register_user(request):
+    pass
+
+
+def logout_user(request):
+    pass
+
+
+def profile_user(request):
+    pass

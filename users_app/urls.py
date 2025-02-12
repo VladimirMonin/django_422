1924b6ba90aa0bcd8c9166bbf5a632566954a7dd
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-02-12 23:09:43 +0500

    lesson_69: Сброс пароля. Сообщение о сбросе и письмо

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from users_app.views import CustomLogoutView, CustomLoginView, RegisterView, ProfileDetailView, ProfileEditView, ProfilePasswordView
-from django.contrib.auth.views import PasswordResetView
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView
 from .forms import CustomPasswordResetForm
 
 app_name = "users"
@@ -18,7 +18,14 @@ urlpatterns = [
          PasswordResetView.as_view(
              template_name='password_reset.html',
              form_class=CustomPasswordResetForm,
-             success_url='/users/password-reset/done/'
+             success_url='/users/password-reset/done/',
+             template_name='password_reset.html'
          ),
          name='password_reset'),
+
+         path('password-reset/done/', 
+         PasswordResetDoneView.as_view(
+             template_name='password_reset_done.html'
+         ),
+         name='password_reset_done'),
 ]
